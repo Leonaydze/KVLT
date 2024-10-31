fb169aa561c001466b6660c4e767f9e2010d1e21
Leo <daadda@gmail.com>
2024-10-31 15:37:54 +0300

    Update

--- KVLT/Enemy.cpp
+++ KVLT/Enemy.cpp
@@ -4,3 +4,29 @@ Enemy::Enemy(int enemyHealth, int enemyDamage){
 	_enemyHealth = enemyHealth;
 	_enemyDamage = enemyDamage;
 }
+
+float Enemy::GetEnemyPosX(){
+	return _enemyPosition.x;
+}
+
+void Enemy::SetEnemyPosX(float enemyPosX){
+	_enemyPosition.x = enemyPosX;
+}
+
+float Enemy::GetEnemyPosY(){
+	return _enemyPosition.y;
+}
+
+void Enemy::SetEnemyPosY(float enemyPosY){
+	_enemyPosition.y = enemyPosY;
+}
+
+Enemy::_enemyClass Enemy::GetEnemyClass()
+{
+	return enemyClass;
+}
+
+void Enemy::EnemyDeath(){
+	
+}
+
--- KVLT/Enemy.h
+++ KVLT/Enemy.h
@@ -1,10 +1,29 @@
-#pragma once
+#ifndef ENEMY_H
+#define ENEMY_H
+#include <raylib.h>
 
 class Enemy{
 protected:
 	int _enemyHealth = 0;
 	int _enemyDamage = 0;
+
+	enum _enemyClass { Default, Vampire, Titan, Roc, Orc, Lich};
+
+	_enemyClass enemyClass = Default;
+
+	Vector2 _enemyPosition;
 public:
 	Enemy(int enemyHealth, int enemyDamage);
+
+	float GetEnemyPosX();
+	void SetEnemyPosX(float enemyPosX);
+
+	float GetEnemyPosY();
+	void SetEnemyPosY(float enemyPosY);
+
+	_enemyClass GetEnemyClass();
+
+	void EnemyDeath();
 };
 
+#endif // !ENEMY_H
